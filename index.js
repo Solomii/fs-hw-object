cd0b60aee11b0769d4aac1e9e08eb4d54609e264
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-01-14 19:06:21 +0200

    Add function

---
+++ index.js
@@ -0,0 +1,87 @@
+/*
+ створити конструктор для Cattle(brand, maxVolume, minVolume)
+brand
+maxVolume
+minVolume
+currentVolume = 0
+isOn = false
+
+addWater(value) //налий води
+makeTea(value)  //відлий воду
+turnOn()  //кип'яти
+ */
+
+function Cattle(brand, maxVolume, minVolume) {
+    this.brand = brand;
+    this.maxVolume = maxVolume;
+    this.minVolume = minVolume;
+    this.currentVolume = 0;
+    this.isOn = false;
+
+    this.addWater = function (value) {
+        if (value < 0) {
+            console.log("error");
+        }
+        if (this.currentVolume + value > maxVolume) {
+            console.log(this.brand + "," + " error overflow");
+            return;
+        } else {
+            this.currentVolume += value;
+        }
+        console.log(
+            this.brand +
+                ", " +
+                "water is added, now cattle has " +
+                this.currentVolume +
+                "ml water"
+        );
+    };
+
+    this.makeTea = function (value) {
+        if (value < 0) {
+            console.log("error");
+        }
+        if (this.isOn === false) {
+            console.log(this.brand + "," + " water is cold");
+            return;
+        }
+
+        if (this.currentVolume - value < 0) {
+            console.log(this.brand + " ," + "error not enough water");
+            return;
+        } else {
+            this.currentVolume -= value;
+        }
+        console.log(
+            this.brand +
+                ", " +
+                "your tea is done, " +
+                this.currentVolume +
+                " is left"
+        );
+    };
+
+    this.turnOn = function () {
+        if (
+            this.currentVolume >= this.minVolume &&
+            this.currentVolume <= this.maxVolume
+        ) {
+            this.isOn = true;
+            console.log(this.brand + ", " + "turned on cattle");
+        }
+    };
+}
+
+const cattel = new Cattle("Braun", 1500, 200);
+const cattel2 = new Cattle("Samsung", 2500, 300);
+const cattel3 = new Cattle("Philips", 1000, 100);
+
+cattel.addWater(400);
+cattel.turnOn();
+cattel.makeTea(350);
+cattel2.addWater(3000);
+cattel2.turnOn();
+cattel2.makeTea(200);
+cattel3.addWater(800);
+cattel3.turnOn();
+cattel3.makeTea(1200);
